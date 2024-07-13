6d2e2e4b340a9f0be58a2fdde9c622354ba2f084
KupoKopu <lukaskuzavas@gmail.com>
2024-07-13 23:41:26 +0100

    KAN-1: separate logger into service

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,8 @@
-import logging
-
 from flask import redirect, render_template, url_for
 
 from app import app
 from app.forms import AddToDoForm
 from app.services import todo_service
-
-logger = logging.getLogger('werkzeug')  # Get the underlying WSGI logger
-handler = logging.FileHandler('app.log')  # Create a handler for the log file
-logger.addHandler(handler)  # Add the handler to the werkzeug logger
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,7 +10,6 @@ logger.addHandler(handler)  # Add the handler to the werkzeug logger
 def index():
 
     todos = todo_service.get_all_todos()
-    logger.info(todos)
 
     return render_template('index.html', title='To Do', todos=todos)
 
--- app/services/todo_service.py
+++ app/services/todo_service.py
@@ -1,9 +1,14 @@
 from app import db
 from app.models import ToDo
+from app.services.logger_service import setup_logger
+
+logger = setup_logger()
 
 
 def get_all_todos():
-    return ToDo.query.all()
+    result = ToDo.query.all()
+    logger.info(f'Getting all from to_do: {result}')
+    return result
 
 
 def add_todo(task, description):
@@ -11,5 +16,8 @@ def add_todo(task, description):
         todo = ToDo(task=task, description=description)
         db.session.add(todo)
         db.session.commit()
+        logger.info(f'Inserted to_do: {todo}')
+
     except Exception as e:
         db.session.rollback()
+        logger.error(f'Error inserting to_do: {e}')
