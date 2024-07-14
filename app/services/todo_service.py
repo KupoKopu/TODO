fd941d78f1fa5e6035914b9aeffd553885520711
KupoKopu <lukaskuzavas@gmail.com>
2024-07-14 22:45:22 +0100

    KAN-1: add exception logging for getting todos

--- app/services/todo_service.py
+++ app/services/todo_service.py
@@ -5,12 +5,6 @@ from app.services.logger_service import setup_logger
 logger = setup_logger()
 
 
-def get_all_todos():
-    result = ToDo.query.all()
-    logger.info(f'Getting all from to_do: {result}')
-    return result
-
-
 def add_todo(task, description):
     try:
         todo = ToDo(task=task, description=description)
@@ -21,3 +15,14 @@ def add_todo(task, description):
     except Exception as e:
         db.session.rollback()
         logger.error(f'Error inserting to_do: {e}')
+
+
+def get_all_todos():
+    try:
+        result = ToDo.query.all()
+        logger.info(f'Getting all from to_do: {result}')
+        return result
+
+    except Exception as e:
+        logger.error(f'Error getting todos: {e}')
+        return []
